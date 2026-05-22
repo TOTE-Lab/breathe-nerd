8b31ffc78dfbba729fa465481c4c6afb9ce8aa83
Tanisha <tthompsonjones8771@gmail.com>
2026-05-21 22:53:29 -0400

    setting up canvas 2d drawing API

--- client/src/components/WaveForm.tsx
+++ client/src/components/WaveForm.tsx
@@ -1,19 +1,55 @@
 import { useRef, useEffect } from "react";
 
 export default function WaveForm() {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    return (
-        <canvas
-            ref={canvasRef}
-            style={{
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                width: '100%',
-                height: '100px',
-                background: '#071A2E'
-            }}
-        />
-    )
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas?.getContext("2d");
+    if (!context) return;
+    let animId: number;
+
+    const waves = [
+      {
+        amplitude: 12,
+        frequency: 0.02,
+        speed: 1.2,
+        color: "rgba(55,138,221,0.3)",
+        offsetY: 0,
+      },
+      {
+        amplitude: 8,
+        frequency: 0.03,
+        speed: 0.8,
+        color: "rgba(55,138,221,0.5)",
+        offsetY: 6,
+      },
+      {
+        amplitude: 10,
+        frequency: 0.025,
+        speed: 1.0,
+        color: "rgba(133,183,235,0.8)",
+        offsetY: 3,
+      },
+    ];
+
+    function draw() {
+        
+    }
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        height: "100px",
+        background: "#071A2E",
+      }}
+    />
+  );
 }
